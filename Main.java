0750d37d4a7b0c89c4d43fba77ac5c276424e48e
Nal Bilalova <59770118+bnfnal@users.noreply.github.com>
2022-04-15 13:19:37 +0300

    Update Main.java
    Task 3: Take a Ten Minutes Walk

--- Main.java
+++ Main.java
@@ -23,12 +23,23 @@ public class Main {
         
         //Task 2: Break camelCase
         
-        Scanner s = new Scanner(System.in);
+        Scanner s1 = new Scanner(System.in);
         System.out.println("Task 2: Break camelCase");
         System.out.println("Введите слово в camelCase");
-        String str = s.next();
+        String str1 = s1.next();
         System.out.println("Отформатированная строка:");
-        System.out.println(BreakCamelCase.camelCase(str));
+        System.out.println(BreakCamelCase.camelCase(str1));
+        System.out.println();
+        
+        //Task 3: Take a Ten Minutes Walk
+        
+        Scanner s2 = new Scanner(System.in);
+        System.out.println("Task 3: Take a Ten Minutes Walk");
+        System.out.println("Введите строку направлений ходьбы состоящую из букв n, s, e, w без пробелов");
+        String str2 = s2.next();
+        char[] walk = str2.toCharArray();
+        System.out.println("Займет ли прогулка ровно десять минут и вернетесь ли вы в исходную точку?");
+        System.out.println(TenMinWalk.isValid(walk));
         System.out.println();
     }
 
@@ -65,5 +76,29 @@ public class Main {
           return res;
        }
     }
+    
+    //Task 3: Take a Ten Minutes Walk
+    
+    public static class TenMinWalk 
+    {
+       public static boolean isValid(char[] walk) 
+       {
+          if (walk.length == 10)
+          {
+             int ns = 0;
+             int ew = 0;
+             for (int i = 0; i< walk.length; i++) 
+             {
+                if (walk[i] == 'n') ns +=1;
+                if (walk[i] == 's') ns -=1;
+                if (walk[i] == 'e') ew +=1;
+                if (walk[i] == 'w') ew -=1;
+             }
+             if ( ns == 0 && ew == 0) return true;
+             else return false;
+         }
+         else return false;
+      }
+   }
         
 }
